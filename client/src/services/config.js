a5305846dc2333d0e8662986f00df6c0d0621930
Aditi Maurya <shally.aditi23@gmail.com>
2023-06-06 18:48:30 +0530

    Update files

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,4 +1,6 @@
 const apiUrl =
-  process.env.NODE_ENV === "production" ? null : "http://localhost:5000"
+  process.env.NODE_ENV === "production"
+    ? "https://aegiscovenent-server.onrender.com"
+    : "http://localhost:5000"
 
 export { apiUrl }
